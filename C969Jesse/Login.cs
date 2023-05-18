d1c286af02072604b2d2825c643deeab39c0ff78
Jesse Perez <jesseorhs@gmail.com>
2023-05-18 20:25:08 +0000

    Add proper login notifications in english and spanish

--- C969Jesse/Login.cs
+++ C969Jesse/Login.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,22 +28,43 @@ namespace C969Jesse
 
 		private void btnLogin_Click(object sender, EventArgs e)
 		{
-			var conn = new MySqlConnection();
-			conn.ConnectionString = ConfigurationManager.ConnectionStrings["localdb"].ConnectionString;
-			conn.Open();
+			try
+			{
+				var conn = new MySqlConnection();
+				conn.ConnectionString = ConfigurationManager.ConnectionStrings["localdb"].ConnectionString;
+				conn.Open();
 
-			string username = txtUserLogin.Text;
-			string password = txtUserPassword.Text;
-			string query = $"SELECT * FROM user WHERE userName='{username}' AND password='{password}'";
-			var command = new MySqlCommand(query, conn);
-			var reader = command.ExecuteReader();
-			if (reader.HasRows)
-			{
-				MessageBox.Show("Login succcessful!");
+				string username = txtUserLogin.Text;
+				string password = txtUserPassword.Text;
+				string query = $"SELECT * FROM user WHERE userName='{username}' AND password='{password}'";
+				var command = new MySqlCommand(query, conn);
+				var reader = command.ExecuteReader();
+				if (reader.HasRows)
+				{
+					if (CultureInfo.CurrentCulture.TwoLetterISOLanguageName == "en")
+					{
+						MessageBox.Show("Login succcessful!");
+					}
+					else if (CultureInfo.CurrentUICulture.TwoLetterISOLanguageName == "es")
+					{
+						MessageBox.Show("Inicio de sesion con exito");
+					}
+				}
+				else
+				{
+					if (CultureInfo.CurrentCulture.TwoLetterISOLanguageName == "en")
+					{
+						MessageBox.Show("Username or Password is incorrect");
+					}
+					else if (CultureInfo.CurrentUICulture.TwoLetterISOLanguageName == "es")
+					{
+						MessageBox.Show("El nombre de usuario o la contrasena son incorrectos");
+					}
+				}
 			}
-			else
+			catch (MySqlException)
 			{
-				MessageBox.Show("Invalid username or password");			
+				MessageBox.Show("Server connection error");
 			}
 
 
